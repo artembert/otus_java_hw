12553115415f715e3a71f0e3c52dfcc63da7f234
Artem Beresnev <artem.beresnev@collectly.co>
2024-07-10 17:44:14 +0300

    HW-1: fix the main() method signature

--- hw01-gradle/src/main/java/com/artembert/HelloOtus.java
+++ hw01-gradle/src/main/java/com/artembert/HelloOtus.java
@@ -4,7 +4,7 @@ import com.google.common.base.Joiner;
 
 @SuppressWarnings("java:S106")
 public class HelloOtus {
-   public static void main() {
+    public static void main(String... args) {
         Joiner joiner = Joiner.on("; ").skipNulls();
         String characters = joiner.join("Harry", null, "Ron", "Hermione");
         System.out.println(characters);
