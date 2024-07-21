c16d8ac77169e47c145fc33a9c0836a81ede2b44
Artem Beresnev <artem.beresnev@collectly.co>
2024-07-21 13:10:06 +0300

    HW-2: remove comment

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -5,8 +5,6 @@ import java.util.LinkedList;
 public class CustomerReverseOrder {
     private final LinkedList<Customer> collection = new LinkedList<>();
 
-    // надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-
     public void add(Customer customer) {
         this.collection.addFirst(customer);
     }
