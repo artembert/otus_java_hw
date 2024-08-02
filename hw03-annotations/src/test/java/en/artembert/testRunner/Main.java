bb0e2d5fa407981c9eff69f5b80883b1543cde32
Artem Beresnev <artem.beresnev@collectly.co>
2024-08-02 07:02:21 +0300

    HW-3: add failed test

--- hw03-annotations/src/test/java/en/artembert/testRunner/Main.java
+++ hw03-annotations/src/test/java/en/artembert/testRunner/Main.java
@@ -1,9 +1,11 @@
 package en.artembert.testRunner;
 
+import en.artembert.testRunner.tests.FailedTest;
 import en.artembert.testRunner.tests.SuccessfulTest;
 
 public class Main {
     public static void main(String[] args) {
         Runner.run(SuccessfulTest.class);
+        Runner.run(FailedTest.class);
     }
 }
--- hw03-annotations/src/test/java/en/artembert/testRunner/tests/FailedTest.java
+++ hw03-annotations/src/test/java/en/artembert/testRunner/tests/FailedTest.java
@@ -1,4 +1,33 @@
 package en.artembert.testRunner.tests;
 
+import en.artembert.testRunner.annotations.After;
+import en.artembert.testRunner.annotations.Before;
+import en.artembert.testRunner.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FailedTest {
+    private int globalVariable = 0;
+
+    @Before
+    public void setUp() {
+        globalVariable = 5;
+    }
+
+    @Test
+    public void equalToConstantTest() {
+        int newValue = globalVariable + 1 - 1;
+        assertEquals(6, newValue);
+    }
+
+    @Test
+    public void simpleSumConstantTest() {
+        assertEquals(2, 1 + 1 + 1);
+    }
+
+    @After
+    public void tearDown() {
+        assertTrue(true);
+    }
 }
